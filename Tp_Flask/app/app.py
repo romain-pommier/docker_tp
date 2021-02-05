2b6ec4145a0d44e381540fcde5092e06f2bbb970
romain <romain-p31@hotmail.fr>
2021-02-05 17:26:39 +0100

    get books

--- Tp_Flask/app/app.py
+++ Tp_Flask/app/app.py
@@ -1,5 +1,5 @@
 import sys, os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -13,6 +13,21 @@ def index():
 @app.route("/mon_nom/<name>")
 def MyCustomName(name):
     return render_template('hello.html', name=name)
+
+@app.route('/api/books', methods = ['GET'])
+def GetBook():
+    book=[
+        {
+            'id':1,
+            'titre' : 'un titre',
+        },
+        {
+            'id':2,
+            'titre': 'un autre titre random',
+        }
+    ]
+    return jsonify(book)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
