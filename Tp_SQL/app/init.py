0f9c633f30a0f641329989037fc54e759f68a9c3
romain-pommier <romain-p31@hottmail.fr>
2021-02-17 18:20:02 +0100

    init tp sql Docker

---
+++ Tp_SQL/app/init.py
@@ -0,0 +1,28 @@
+
+from SQLAlchemy import SQLAlchemy
+
+from .models import db
+
+print('coucou')
+
+config = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'newuser',
+    'password': 'newpassword',
+    'database': 'classicmodels'
+}
+
+db_user = config.get('user')
+db_pwd = config.get('password')
+db_host = config.get('host')
+db_port = config.get('port')
+db_name = config.get('database')
+
+
+connection_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
+
+engine = create_engine(connection_str)
+connection = engine.connect()
+print('------MYSQL Docker Container Python connection ok -----')
+
